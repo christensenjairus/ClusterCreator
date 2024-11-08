89ccd861deecfc92742ee77855849167ffd5f60f
Jairus Christensen <christensenjairus@gmail.com>
2024-11-08 15:27:46 -0700

    Refactor scripts to rely on clustercreator.sh and it's exported functions + variables.

--- scripts/uninstall_k8s.sh
+++ scripts/uninstall_k8s.sh
@@ -1,52 +1,58 @@
 #!/bin/bash
 
-# Initialize default cluster name
-CLUSTER_NAME=""
-SINGLE_HOSTNAME="*"
+usage() {
+  echo "Usage: clustercreator.sh|ccr uninstall-k8s [-n/--node <hostname>]"
+}
+
+TARGETED_NODE="*"
 
 # Parse command-line arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -n|--cluster-name) CLUSTER_NAME="$2"; shift ;;
-        -h|--single-hostname) SINGLE_HOSTNAME="$2"; shift ;;
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+        -n|--node) TARGETED_NODE="$2"; shift ;;
+        -h|--help) usage; exit 0 ;;
+        *) echo "Unknown parameter passed: $1"; usage; exit 1 ;;
     esac
     shift
 done
 
-if [[ -z "$CLUSTER_NAME" ]]; then
-    echo "Error: CLUSTER_NAME is required."
-    echo "Usage: $0 -n/--cluster-name <CLUSTER_NAME> [-h/--single-hostname <SINGLE_HOSTNAME>]"
-    exit 1
-fi
-
-echo "Running ansible on cluster: $CLUSTER_NAME"
-
-# Get the directory where the script is located
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 set -e
 trap 'echo "An error occurred. Cleaning up..."; cleanup_function' ERR
-pushd "$SCRIPT_DIR" || exit
-
-source .env
-source k8s.env
-
-cd ../ansible || exit
-
 cleanup_function() {
   rm -f \
     "tmp/${CLUSTER_NAME}/worker_join_command.sh" \
     "tmp/${CLUSTER_NAME}/control_plane_join_command.sh" \
     >&/dev/null
-  popd || true
-  echo "Cleanup complete."
 }
+
+check_required_vars "REPO_PATH"
+cd "$REPO_PATH/scripts"
+
+set -a # automatically export all variables
+source .env
+source k8s.env
+set +a # stop automatically exporting
+
+required_vars=(
+  "VM_USERNAME"
+  "NON_PASSWORD_PROTECTED_SSH_KEY"
+  "KUBERNETES_MEDIUM_VERSION"
+  "CLUSTER_NAME"
+  "TARGETED_NODE"
+)
+
+check_required_vars "${required_vars[@]}"
+print_env_vars "${required_vars[@]}"
+
+cd "$REPO_PATH/ansible"
 
 ## run ansible playbooks
 ansible-playbook -i "tmp/${CLUSTER_NAME}/ansible-hosts.txt" -u "$VM_USERNAME" uninstall-cluster-playbook.yaml \
   --private-key "$HOME/.ssh/${NON_PASSWORD_PROTECTED_SSH_KEY}" \
   -e "cluster_name=${CLUSTER_NAME}" \
-  --limit="${SINGLE_HOSTNAME}"
+  --limit="${TARGETED_NODE}"
 
 # cleanup join commands
 cleanup_function
+
+echo -e "${GREEN}K8S UNINSTALL COMPLETE${ENDCOLOR}"
